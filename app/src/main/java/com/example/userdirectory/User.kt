f75f7233b28989a6ae6e1d913aaf9bd4097544fe
Romushkin <romushkins@gmail.com>
2024-12-07 23:23:42 +0300

    update

--- app/src/main/java/com/example/userdirectory/User.kt
+++ app/src/main/java/com/example/userdirectory/User.kt
@@ -1,3 +1,7 @@
 package com.example.userdirectory
 
-data class User(private val name: String, private val age: Int)
+class User(private val name: String, private val age: Int) {
+    override fun toString(): String {
+        return "Имя: $name, возраст: $age"
+    }
+}
